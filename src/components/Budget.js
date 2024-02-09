271a9b283c2695b28b9fd562049c32747eca3159
ronennaz3 <ronennaz3@gmail.com>
2024-02-09 15:06:26 -0500

    with Nikesh correction

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,14 +3,12 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
+    const [newBudget, changeBudget] = useState(budget);
+    
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<span>Budget: £</span>
+<input type="number" step="10" value={newBudget} onInput={(event) => changeBudget(event.target.value)}></input>
 </div>
     );
 };
